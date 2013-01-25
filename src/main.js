84631d9f5ee2a7de50b7cf42bc214f8e1eccfb2d
Daniel Holmlund <agnathan@gmail.com>
2013-01-25 14:30:02 -0800

    Integrated RequireJS

---
+++ src/main.js
@@ -0,0 +1,13 @@
+require.config({
+    baseUrl: 'features-json'
+    // waitSeconds : 2,
+    paths : {
+        text : '../lib/text', //text is required
+        json : '../src/json' //alias to plugin
+    }
+});
+
+// adding the flag `!bust` to the end of dependency name will avoid caching
+require(['json!audio.json'], function(feature){
+    console.log(feature.title)
+});
